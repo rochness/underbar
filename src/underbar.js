442d3ebd087e249f1043f3c9030ff5a642b73eae
Rochelle Lee <rochelle.y.lee@gmail.com>
2015-08-17 17:38:13 -0700

    Completed cod for _.filter function to pass tests

--- src/underbar.js
+++ src/underbar.js
@@ -82,6 +82,15 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(element){
+      if(test(element)){
+        result.push(element);
+      }
+    });
+
+    return result;
+  
   };
 
   // Return all elements of an array that don't pass a truth test.
