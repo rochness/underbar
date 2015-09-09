e1edb50f9c509f0fd9b067d8c8a2981836cbb91d
Rochelle Lee <rochelle.y.lee@gmail.com>
2015-09-09 15:41:29 -0700

    Completed code for _.defaults function to pass all tests

--- src/underbar.js
+++ src/underbar.js
@@ -279,6 +279,16 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+  	if(arguments.length>1){
+    		for(var i=1; i<arguments.length; i++){
+    			_.each(arguments[i], function(value, key){
+    				if(!obj.hasOwnProperty(key)){
+    					obj[key] = value;
+    				}
+    			});
+    		}
+    	}
+    	return obj;
   };
 
 
