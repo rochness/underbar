56df17df565550a2a1e3bdc30a5ccdd1cc67d62c
Rochelle Lee <rochelle.y.lee@gmail.com>
2015-09-09 17:43:12 -0700

    Completed code for _.delay function to pass all tests

--- src/underbar.js
+++ src/underbar.js
@@ -359,6 +359,11 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+  		var funcArgs = [];
+  		for(var i = 2; i<arguments.length; i++){
+  			funcArgs.push(arguments[i]);
+  		}
+  		setTimeout(function(){return func.apply(null, funcArgs);}, wait);
   };
 
 
